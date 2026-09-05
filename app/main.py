a05013965e16a10f046cc6b6a2afbc0dbc6d3ad7
Bulut Altundiş <bulutaltundis@Buluts-MacBook-Air.local>
2026-09-05 15:41:14 +0300

    added something

--- app/main.py
+++ app/main.py
@@ -72,9 +72,14 @@ def startup():
 
 
 @app.get("/")
-def home():
+def home(request: Request, session: Session = Depends(get_session)):
+    if get_current_user(request, session):
+        return RedirectResponse(
+            url="/dashboard",
+            status_code=303,
+        )
     return RedirectResponse(
-        url="/dashboard",
+        url="/billing",
         status_code=303,
     )
 
